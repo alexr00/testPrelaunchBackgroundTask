a2d85094e63889e02b967ee7c7142dd299aae2a1
Alex Ross <alros@microsoft.com>
2022-02-28 11:16:11 +0100

    Enable sample for linux

---
+++ .vscode/fakeScript.sh
@@ -0,0 +1,32 @@
+echo off
+echo "Open On-Chip Debugger  v0.11.0-esp32-20211220 (2021-12-20-15:42)"
+echo "Licensed under GNU GPL v2"
+echo "For bug reports, read"
+echo "        http://openocd.org/doc/doxygen/bugs.html"
+echo "adapter speed: 20000 kHz"
+echo "Info : Listening on port 6666 for tcl connections"
+echo "Info : Listening on port 4444 for telnet connections"
+echo "Info : ftdi: if you experience problems at higher adapter clocks, try the command 'ftdi_tdo_sample_edge falling'"
+echo "Info : clock speed 20000 kHz"
+echo "Info : JTAG tap: esp32.cpu0 tap/device found: 0x120034e5 (mfg: 0x272 (Tensilica), part: 0x2003, ver: 0x1)"
+echo "Info : JTAG tap: esp32.cpu1 tap/device found: 0x120034e5 (mfg: 0x272 (Tensilica), part: 0x2003, ver: 0x1)"
+echo "Info : starting gdb server for esp32.cpu0 on 3333"
+sleep 3
+echo "Info : Listening on port 3333 for gdb connections"
+
+sleep 10
+
+echo "Open On-Chip Debugger  v0.11.0-esp32-20211220 (2021-12-20-15:42)"
+echo "Licensed under GNU GPL v2"
+echo "For bug reports, read"
+echo "        http://openocd.org/doc/doxygen/bugs.html"
+echo "adapter speed: 20000 kHz"
+echo "Info : Listening on port 6666 for tcl connections"
+echo "Info : Listening on port 4444 for telnet connections"
+echo "Info : ftdi: if you experience problems at higher adapter clocks, try the command 'ftdi_tdo_sample_edge falling'"
+echo "Info : clock speed 20000 kHz"
+echo "Info : JTAG tap: esp32.cpu0 tap/device found: 0x120034e5 (mfg: 0x272 (Tensilica), part: 0x2003, ver: 0x1)"
+echo "Info : JTAG tap: esp32.cpu1 tap/device found: 0x120034e5 (mfg: 0x272 (Tensilica), part: 0x2003, ver: 0x1)"
+echo "Info : starting gdb server for esp32.cpu0 on 3333"
+sleep 3
+echo "Info : Listening on port 3333 for gdb connections"
